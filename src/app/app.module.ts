c90927c24689d52ec428d46d2a20d06d6e78bd48
Franco <fraffa1990@gmail.com>
2024-09-04 14:55:09 -0300

    Project Structure/Scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SelectorPageComponent } from './countries/pages/selector-page/selector-page.component';
+import { CountriesModule } from './countries/countries.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CountriesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
